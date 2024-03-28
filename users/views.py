de1097cd395647063becd84c1fb5a451feb0f3dd
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-03-28 08:02:38 +0500

    last commit for 28-march

--- users/views.py
+++ users/views.py
@@ -11,7 +11,7 @@ from rest_framework.decorators import permission_classes
 from datetime import datetime, timedelta
 from rest_framework.validators import ValidationError
 from rest_framework.response import Response
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
 
@@ -170,6 +170,7 @@ class LoginView(TokenObtainPairView):
 
 
 
-
+class LoginRefreshView(TokenRefreshView):
+    serializer_class = LoginRefreshSerializer
 
 

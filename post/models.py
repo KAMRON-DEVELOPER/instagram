baae458e597e08ddc9ef07c41d523d22414c5dc9
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-03-29 09:29:06 +0500

    last commit for 29-march

--- post/models.py
+++ post/models.py
@@ -3,6 +3,8 @@ from users.models import User
 from django.contrib.auth import get_user_model
 from shared.models import BaseModel
 from django.core.validators import FileExtensionValidator, MaxLengthValidator
+from django.db.models import UniqueConstraint
+
 
 
 class Post(BaseModel):
@@ -14,8 +16,31 @@ class Post(BaseModel):
     
     class Meta:
         db_table = "posts" # default post_post 1-app name, 2-model name
+        verbose_name = "post"
+        verbose_name_plural = "posts"
         
     def __str__(self):
         return self.title
     
+
+
+class PostComment(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments") # Post.comments.all() all comments related one Post
+    comment = models.TextField()
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children', null=True, blank=True)
+
+
+
+
+class PostLike(BaseModel):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    
+    class Meta:
+        constrains = [
+            
+        ]
+    
+
 

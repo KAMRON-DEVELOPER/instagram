c29a74fb2d8de110826f924f2991cf157bbdeb47
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-03-29 09:58:40 +0500

    last commit for 29-march

--- post/serializers.py
+++ post/serializers.py
@@ -1,14 +1,34 @@
 from rest_framework import serializers
+
+from users.models import User
 from .models import Post, PostComment, PostLike, CommentLike
 
 
 
+class UserSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'date_of_birth', 'user_gender']
+
+
 
 class PostSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(~)
+    id = serializers.UUIDField(read_only=True)
+    author = UserSerializer(read_only=True)
+    post_likes_count = serializers.SerializerMethodField('get_post_likes_count')
+    post_comments_count = serializers.SerializerMethodField('get_post_comments_count')
     
     class Meta:
         model = Post
-        fields = ['id', 'title', 'author', 'body', 'post_image', 'created_time']
+        fields = ['id', 'title', 'author', 'body', 'post_image', 'created_time', 'post_likes']
+
+    def get_post_likes_count(self, obj):
+        return obj.post_likes.count()
+    
+    def get_post_comments_count(self, obj):
+        return obj.post_likes.count()
+
+
 
 

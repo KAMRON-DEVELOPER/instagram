d098a301b9123f9f63f06956fade1029a8616d1c
KAMRON-DEVELOPER <atajanovkamronbek2003@gmail.com>
2024-04-01 17:21:33 +0500

    last commit for 1-aprel

--- post/views.py
+++ post/views.py
@@ -3,12 +3,14 @@ from .models import Post, PostComment, PostLike, CommentLike
 from .serializers import PostSerializer, PostCommentSerializer, CommentLikeSerializer, PostLikeSerializer
 from rest_framework.generics import ListAPIView, CreateAPIView, DestroyAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.views import APIView
+from rest_framework import permissions
 
 
 
 
 class PostListAPIView(ListAPIView):
     
+    permission_classes = [permissions.AllowAny,]
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
